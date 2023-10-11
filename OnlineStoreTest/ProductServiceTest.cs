93c347506d1dc4d360430c07b6a5ab91f971c60f
nargesmoosavi <nrgmsv@gmail.com>
2023-10-11 10:44:08 +0330

    unit test for NameIsNotUnique

--- OnlineStoreTest/ProductServiceTest.cs
+++ OnlineStoreTest/ProductServiceTest.cs
@@ -9,12 +9,15 @@ namespace OnlineStoreTest
         public void Add_NameIsNotUnique_ReturnsException()
         {
             //Arrange
+            var product = new ProductViewModel {Title="test" };
             var stubProdRepository = new Mock<IProductRepository>();
             var stubUserRepository = new Mock<IUserRepository>();
-            stubProdRepository.Setup(p => p.GetByTitleAsync("")).Returns(Task.FromResult<ProductViewModel?>(new ProductViewModel { }));
+            stubProdRepository.Setup(p => p.GetByTitleAsync("test")).Returns(Task.FromResult<ProductViewModel?>(product));
             var stubProductService = new ProductService(stubProdRepository.Object, stubUserRepository.Object);
-            //Act
-            //Assert
+            //Act - assert
+            var ex =
+                Assert.ThrowsAsync<ArgumentException>(() => stubProductService.AddAsync(product));
+            Assert.Equal("Product title must be unique.", ex.Result.Message);
         }
         [Fact]
         public void Add_NameMoreThan40Char_ReturnsException()
@@ -30,20 +33,6 @@ namespace OnlineStoreTest
             var ex = 
                 Assert.ThrowsAsync<ArgumentException>(() => stubProductService.AddAsync(product));
             Assert.Equal("Product title must be less than 40 characters.", ex.Result.Message);
-        }
-        [Fact]
-        public void Buy_AddProductToUserOrders_ReturnsUserOrder()
-        {
-            //Arrange
-            //Act
-            //Assert
-        }
-        [Fact]
-        public void Buy_DecreseTheInventory_ReturnsOneItem()
-        {
-            //Arrange
-            //Act
-            //Assert
         }
     }
 }
